97eb3f76fe5997851ab2a5371413ffaf3ed25028
yangzhao <1514784049@qq.com>
2025-04-10 15:51:40 +0800

    feat(): 引入 grafana

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -16,7 +16,7 @@ import (
 func NewGRPCServer(c *conf.Server, greeter *service.GreeterService, user *service.UserService, metricsData *pkg.MetricsData) *grpc.Server {
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
-			recovery.Recovery(),
+			recovery.Recovery(), //自动捕获 panic 确保线上服务不崩溃，测试环境应当尽可能让崩溃
 			tracing.Server(),
 			metrics.Server(
 				metrics.WithSeconds(metricsData.Seconds),
--- internal/server/http.go
+++ internal/server/http.go
@@ -17,7 +17,7 @@ import (
 func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, user *service.UserService, metricsData *pkg.MetricsData) *http.Server {
 	var opts = []http.ServerOption{
 		http.Middleware(
-			recovery.Recovery(),
+			recovery.Recovery(), //自动捕获 panic 确保线上服务不崩溃，测试环境应当尽可能让崩溃
 			tracing.Server(),
 			metrics.Server(
 				metrics.WithSeconds(metricsData.Seconds),
@@ -38,6 +38,7 @@ func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, user *servic
 
 	srv := http.NewServer(opts...)
 
+	// Prometheus定期访问http://host.docker.internal:8010/metrics, 抓取收集的指标
 	srv.Handle("/metrics", promhttp.Handler())
 
 	v1.RegisterGreeterHTTPServer(srv, greeter)
